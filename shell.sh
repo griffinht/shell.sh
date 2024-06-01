a614508d1c37c498aae3410aa15d89c88dbf9edb
griffinht <git@griffinht.com>
2024-06-01 16:26:13 -0400

    ayyoo

--- shell.sh
+++ shell.sh
@@ -1,51 +1,44 @@
 #!/usr/bin/env bash
 
-set -e
-set -o errexit
-set -o pipefail
-set -o nounset
+debug_pipe() {
+    cat > /dev/stderr
+}
+
+debug() {
+    echo "[${shell_directory:--}] [${shell_env_file:--}]" "$@" | debug_pipe
+}
+
+setup() {
+    set -o errexit
+    set -o pipefail
+    set -o nounset
+}
+setup
 # todo make everything local
 
 # hooks
-#
-load_import() {
-    # usage load_import directory
-    unset_shell
-    local shell_directory="$1"
-
-    if [ -z "${SHELL_IMPORTS+x}" ]; then
-        return 0
-    fi
-    local IFS=':'
-    shell_imports="$SHELL_IMPORTS"
-    unset SHELL_IMPORTS
-    for shell_import in $shell_imports; do
-        echo LOAD IMPORT "$shell_directory" "$shell_import"
-        import "$shell_directory/$shell_import"
-    done
-}
 
 # todo make this a login hook
 load_bin() {
+    setup
     # usage load_bin directory
-    unset_shell
     local shell_directory="$1"
 
     local bin_dir
     bin_dir="$(readlink -f "$shell_directory/$SHELL_BIN_DIR")"
     #bin_dir="$(readlink -f "$SHELL_BIN_DIR")"
-    echo BIN DIR "$shell_directory" "$bin_dir"
     if [ -d "$bin_dir" ]; then
-        echo loading scripts in "$SHELL_BIN_DIR"
+        debug loading scripts in "$SHELL_BIN_DIR"
         # scripts in bin_dir should override regular scripts
         #export PATH="$PATH:$bin_dir"
-        export PATH="$bin_dir:$PATH"
+        echo "$bin_dir:$PATH"
         # todo supress this output
-        echo
-        ls -h "$bin_dir"
-        echo
+        echo | debug_pipe
+        ls -h "$bin_dir" | debug_pipe
+        echo | debug_pipe
+    else
+        echo "$PATH"
     fi
-    unset "$SHELL_BIN_DIR"
 }
 
 unset_shell() {
@@ -54,12 +47,14 @@ unset_shell() {
 }
 
 import() {
-    unset_shell
+    setup
     local shell_target="$1"
-    local shell_directory
+    local shell_directory=asd
+    unset shell_env_file
 
     if [ -d "$shell_target" ]; then
         shell_directory="$shell_target"
+        #debug no exists? "$shell_directory"
         # in case it was set above
         #unset env_file
         #env_file="$directory/$SHELL_ENV_FILE"
@@ -68,13 +63,31 @@ import() {
         shell_directory="$(dirname "$shell_target")"
         local shell_env_file
         shell_env_file="$(basename "$shell_target")"
+        #debug exists? "$shell_env_file"
     fi
 
-    load() {
-        unset_shell
-        local shell_directory="$1"
-        local shell_env_file="$2"
+    post_hooks() {
+        # todo pass and unset - what is this
+        # todo order should not matter - but it does!
+        export PATH
+        PATH="$(load_bin "$shell_directory")"
+        unset "$SHELL_BIN_DIR"
 
+        # usage load_import directory
+        if [ -z "${SHELL_IMPORTS+x}" ]; then
+            return 0
+        fi
+        local IFS=':'
+        shell_imports="$SHELL_IMPORTS"
+        unset SHELL_IMPORTS
+        for shell_import in $shell_imports; do
+            debug importing \""$shell_import"\"
+            import "$shell_directory/$shell_import" | debug_pipe
+            #debug imports loaded
+        done
+    }
+
+    load() {
         if [ -f "$shell_directory/$shell_env_file" ]; then
             local shell_old_dir="$PWD"
             cd "$shell_directory"
@@ -84,43 +97,27 @@ import() {
             cd "$shell_old_dir"
         fi
 
-        post_hooks "$shell_directory"
-    }
-
-    post_hooks() {
-        unset_shell
-        local shell_directory="$1"
-
-        # todo pass and unset - what is this
-        # todo order should not matter - but it does!
-        local special="$shell_directory"
-        load_bin "$shell_directory"
-        # todo explicit pass and unset
-        load_import "$special"
-        unset special
+        post_hooks
     }
 
     # todo test if env_file ends with shell env file
     if [ -z "${shell_env_file+x}" ]; then
-        echo LOAD DEFAULT
+        debug LOAD DEFAULT
         local shell_env_file="$SHELL_ENV_FILE"
-        load "$shell_directory" "$shell_env_file"
+        load
         # no worries if SHELL_ENV_FILE doesn't exist
     else
-        echo LOAD EXPLICIT
+        debug LOAD EXPLICIT
         # must exist
         if [ ! -f "$shell_directory/$shell_env_file" ]; then
-            echo env file \""$shell_env_file"\" in dir \""$shell_directory"\" does not exist
+            debug env file \""$shell_env_file"\" in dir \""$shell_directory"\" does not exist
             exit 1
         fi
 
-        local special2="${special2:-$shell_directory}"
-        load "$shell_directory" "$shell_env_file"
+        load
     fi
     # todo post load hooks
-    shell_return_directory="$special2"
-    shell_return_environment="${shell_env_file:-default}"
-    #unset special2
+    echo "${shell_directory?}:${shell_env_file:-default}"
 }
 
 
@@ -139,6 +136,7 @@ SHELL_BIN_DIR=bin
 # echo shell.sh no version yet
 
 shell.sh() {
+    setup
     # default to current directory
     declare -r default_target=.
     local target="$SHELL_ENV_FILE"
@@ -156,10 +154,9 @@ shell.sh() {
         fi
     fi
 
-    import "$target"
-    local shell_directory="$shell_return_directory"
-    local shell_environment="$shell_return_environment"
-    unset ${!shell_return_*}
+    local IFS=':'
+    read -r shell_directory shell_environment <<< "$(import "$target")"
+    #read -r shell_directory shell_environment <<< "$(import "$target")"
     # todo make sure everything was local and nothing propogated up?
 
     # run a command
@@ -172,9 +169,9 @@ shell.sh() {
     # run interactively
     shell_directory="$(basename "$(realpath "$shell_directory")")"
     # todo this looks wrong
-    echo entering "$shell_directory" "$shell_environment"
+    debug entering "$shell_directory" "$shell_environment"
     # todo should this have a command? command "$SHELL
     "$SHELL"
-    echo exiting "$shell_directory" "$shell_environment"
+    debug exiting "$shell_directory" "$shell_environment"
 }
 shell.sh "$@"
